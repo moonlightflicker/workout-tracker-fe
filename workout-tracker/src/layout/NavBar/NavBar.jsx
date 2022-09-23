3958e48bed766feb3d383047428f17b2013275ce
César Cachulo <cesarcachulo95@live.com.pt>
2022-09-23 22:16:17 +0100

    Navbar logo animation when hovering properly implemented

--- workout-tracker/src/layout/NavBar/NavBar.jsx
+++ workout-tracker/src/layout/NavBar/NavBar.jsx
@@ -1,8 +1,9 @@
 import './NavBar.scss';
 import NavItem from './NavItem/NavItem';
 
-import logo from '../../assets/branding/placeholder_logo.png';
-import smallLogo from '../../assets/branding/small_placeholder_logo.png';
+// import logo from '../../assets/branding/placeholder_logo.png';
+import logo from '../../assets/branding/placeholder_logo_test.png';
+// import smallLogo from '../../assets/branding/small_placeholder_logo.png';
 import dashboardIcon from '../../assets/icons/dashboard.svg';
 import workoutsIcon from '../../assets/icons/workouts.svg';
 import exercisesIcon from '../../assets/icons/exercises.svg';
@@ -10,25 +11,31 @@ import plansIcon from '../../assets/icons/plans.svg';
 
 function NavBar() {
   return (
-    <nav className="NavBar fixed h-full w-16 group hover:w-64 duration-300">
-      <div className="w-full flex-row justify-center mt-5 mb-10">
+    <nav className="navbar fixed h-full group duration-300 overflow-hidden">
+      <div className="logo-container flex-row justify-center mt-5 mb-10 h-11 overflow-hidden">
         <img
           alt="Logo"
           src={logo}
-          className="h-11
-            opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+          className="logo min-w-max"
         />
-        <img
+        {/* <img
+          alt="Logo"
+          src={logo}
+          className="h-11 min-w-max
+            opacity-0 group-hover:opacity-100
+            transition-opacity duration-300 absolute"
+        /> */}
+        {/* <img
           alt="smallLogo"
           src={smallLogo}
           className="h-11
-          opacity-100 group-hover:opacity-0 transition-opacity duration-300"
-        />
+          opacity-100 group-hover:opacity-0 transition-opacity duration-300 absolute"
+        /> */}
       </div>
-      <div className="ItemContainer overflow-scroll custom-scrollbar w-full">
+      <div className="item-container overflow-scroll custom-scrollbar w-full">
         <NavItem icon={dashboardIcon} text="Dashboard" selected />
         <NavItem icon={workoutsIcon} text="Workouts" selected={false} />
-        <NavItem icon={exercisesIcon} text="Exercises database" selected={false} />
+        <NavItem icon={exercisesIcon} text="Exercise database" selected={false} />
         <NavItem icon={plansIcon} text="Plans" selected={false} />
       </div>
       {/* TODO: Implemente licensing and socials */}
