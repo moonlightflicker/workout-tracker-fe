8f63f9bf805153953c2323dad36e92f06809f123
César Cachulo <cesarcachulo95@live.com.pt>
2022-09-24 17:26:04 +0100

    Logo creation and necessary style changes

--- workout-tracker/src/layout/NavBar/NavBar.jsx
+++ workout-tracker/src/layout/NavBar/NavBar.jsx
@@ -1,9 +1,7 @@
 import './NavBar.scss';
 import NavItem from './NavItem/NavItem';
 
-// import logo from '../../assets/branding/placeholder_logo.png';
-import logo from '../../assets/branding/placeholder_logo_test.png';
-// import smallLogo from '../../assets/branding/small_placeholder_logo.png';
+import logo from '../../assets/branding/logo.png';
 import dashboardIcon from '../../assets/icons/dashboard.svg';
 import workoutsIcon from '../../assets/icons/workouts.svg';
 import exercisesIcon from '../../assets/icons/exercises.svg';
@@ -18,19 +16,6 @@ function NavBar() {
           src={logo}
           className="logo min-w-max"
         />
-        {/* <img
-          alt="Logo"
-          src={logo}
-          className="h-11 min-w-max
-            opacity-0 group-hover:opacity-100
-            transition-opacity duration-300 absolute"
-        /> */}
-        {/* <img
-          alt="smallLogo"
-          src={smallLogo}
-          className="h-11
-          opacity-100 group-hover:opacity-0 transition-opacity duration-300 absolute"
-        /> */}
       </div>
       <div className="item-container overflow-scroll custom-scrollbar w-full">
         <NavItem icon={dashboardIcon} text="Dashboard" selected />
