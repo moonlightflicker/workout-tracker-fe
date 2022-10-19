1e6fe22db669aa89bd21cc662cff1eea42f88855
moonlightflicker <cesarcachulo95@live.com.pt>
2022-10-19 19:48:19 +0100

    Login page developments

--- workout-tracker/src/pages/Login.jsx
+++ workout-tracker/src/pages/Login.jsx
@@ -1,10 +1,22 @@
 import './Login.scss';
+import backgroundImage from '../assets/backgrounds/login_background.jpg';
 
 function Login() {
   return (
-    <div>
+    <div className="w-screen">
+
       <div className="login-card overflow-hidden">
-        Login
+
+        {/* Background image */}
+        <div className="background-side float-left h-full">
+          <img className="background-image inline-block" alt="Background" src={backgroundImage} />
+        </div>
+
+        {/* Login form */}
+        <div className="login-side float-right h-full">
+          Login
+        </div>
+
       </div>
     </div>
   );
